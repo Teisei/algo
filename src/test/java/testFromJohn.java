57507c5f0ac0b80d8cb0636de822b7ca921d8cf3
teisei <teisei.din@gmail.com>
2015-06-13 21:13:32 +0800

    a test from my another working computer.

---
+++ src/test/java/testFromJohn.java
@@ -0,0 +1,8 @@
+/**
+ * Created by teisei on 6/13/15.
+ */
+public class testFromJohn {
+    public static void main(String args[]) {
+        System.out.println("this is a test class from my laptop John");
+    }
+}
